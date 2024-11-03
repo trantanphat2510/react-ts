78a2b99290135d21184c0cc42097d1eecf0ea001
trantanphat2510 <trantanphat25100110@gmail.com>
2024-11-03 14:11:35 +0700

    'update'

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Xin chào tất cả các bạn, bây giờ tôi mới bắt đầu haizzzzzzzzzzzz
         </p>
       </div>
       <p className="read-the-docs">
